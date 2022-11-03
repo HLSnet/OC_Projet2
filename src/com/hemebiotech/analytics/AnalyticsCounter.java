e9012c059dcfe3e552484f576627dbc126325d19
Said Hadj-Larbi <said.hadjLarbi@bbox.fr>
2022-11-03 14:45:36 +0100

    implementing exception , closing ressource

--- src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -3,6 +3,7 @@ package com.hemebiotech.analytics;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
+import java.io.IOException;
 
 public class AnalyticsCounter {
 	private static int headacheCount = 0;
@@ -10,27 +11,30 @@ public class AnalyticsCounter {
 	private static int pupilCount = 0;
 	
 	public static void main(String args[]) throws Exception {
-		// Opening the file containing the list of the symptoms
-		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
-		
-		// reading the first symptom
-		String line = reader.readLine();
+		try {
+			// Opening the file containing the list of the symptoms
+			BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
+			
+			// reading the first symptom
+			String line = reader.readLine();
+	
+			while (line != null) {
+				System.out.println("symptom from file: " + line);
+				if (line.equals("headache")) {
+					headacheCount++;
+				}
+				else if (line.equals("rash")) {
+					rashCount++;
+				}
+				else if (line.contains("dialated pupils")) {
+					pupilCount++;
+				}
+	
+				line = reader.readLine();	// get another symptom
+			}
+			reader.close();
+		} catch (IOException e) {e.printStackTrace();}			
 
-		while (line != null) {
-			System.out.println("symptom from file: " + line);
-			if (line.equals("headache")) {
-				headacheCount++;
-			}
-			else if (line.equals("rash")) {
-				rashCount++;
-			}
-			else if (line.contains("dialated pupils")) {
-				pupilCount++;
-			}
-
-			line = reader.readLine();	// get another symptom
-		}
-		
 		// generate output
 		FileWriter writer = new FileWriter ("result.out");
 		
