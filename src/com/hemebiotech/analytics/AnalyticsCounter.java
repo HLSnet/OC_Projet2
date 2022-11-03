a93ab33d922786c426ba92760a605a4e0e74b679
Said Hadj-Larbi <said.hadjLarbi@bbox.fr>
2022-11-03 15:11:13 +0100

    counting the symptoms and sorting alphabetically

--- src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -4,13 +4,14 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.TreeMap;
 
 public class AnalyticsCounter {
-	private static int headacheCount = 0;
-	private static int rashCount = 0;
-	private static int pupilCount = 0;
+	public TreeMap<String, Integer> symptoms = new TreeMap<>();
 	
 	public static void main(String args[]) throws Exception {
+		TreeMap<String, Integer> symptoms = new TreeMap<>();
+		
 		try {
 			// Opening the file containing the list of the symptoms
 			BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
@@ -18,29 +19,29 @@ public class AnalyticsCounter {
 			// reading the first symptom
 			String line = reader.readLine();
 	
-			while (line != null) {
-				System.out.println("symptom from file: " + line);
-				if (line.equals("headache")) {
-					headacheCount++;
-				}
-				else if (line.equals("rash")) {
-					rashCount++;
-				}
-				else if (line.contains("dialated pupils")) {
-					pupilCount++;
-				}
-	
-				line = reader.readLine();	// get another symptom
-			}
+
+			
+			// Reading sequentially the file "symptoms.txt"
+						while (line != null) {
+						
+							if  (symptoms.containsKey(line)) {
+								symptoms.put(line, symptoms.get(line)+1);
+							}
+							else {
+								symptoms.put(line, 1);
+							}
+
+							line = reader.readLine();	// get another symptom
+						}
 			reader.close();
+		
 		} catch (IOException e) {e.printStackTrace();}			
 
 		// generate output
-		FileWriter writer = new FileWriter ("result.out");
-		
-		writer.write("headache: " + headacheCount + "\r\n");
-		writer.write("rash: " + rashCount + "\r\n");
-		writer.write("dialated pupils: " + pupilCount + "\r\n");
-		writer.close();
+		try {
+			FileWriter writer = new FileWriter ("result.out");		
+			writer.write(symptoms.toString());
+			writer.close();		
+		} catch (IOException e) {e.printStackTrace();}	
 	}
 }
