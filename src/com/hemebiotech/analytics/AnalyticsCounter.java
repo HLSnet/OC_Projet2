1b808ebc34f805a541a65e433fd4654b4c940336
Said Hadj-Larbi <said.hadjLarbi@bbox.fr>
2022-11-27 15:51:43 +0100

    mise en forme finale

--- src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,79 +1,88 @@
 package com.hemebiotech.analytics;
+
 /**
  * 
  * @author Said
  *
  */
-public class AnalyticsCounter  {
-	private ISymptomReader iSymptomReader = null;	  // used to specify which type of input file 
-	private ISymptomCounter iSymptomCounter = null;	  // used to specify how to count the symptoms
-	private ISymptomWriter iSymptomWriter = null;	  //used to specify which type of output file
-	
+public class AnalyticsCounter {
+	private ISymptomReader iSymptomReader = null; // used to specify which type of input file
+	private ISymptomCounter iSymptomCounter = null; // used to specify how to count the symptoms
+	private ISymptomWriter iSymptomWriter = null; // used to specify which type of output file
+
 	private String inputPath;
 	private String outputPath;
-	
-	
+
 	/**
-	 *  Reading , counting , writing
+	 * Reading , counting , writing
 	 * 
 	 */
 	public void writeAnalysis() {
 		// Writing in the output file,
 		this.iSymptomWriter.saveSymptoms(
-				// the counting of symptoms 
+				// the counting of symptoms
 				this.iSymptomCounter.countSymptoms(
 						// read from the input file.
-						this.iSymptomReader.getSymptoms()
-				)	
-		);
+						this.iSymptomReader.getSymptoms()));
 	}
-	
+
 	// CONSTRUCTORS
+	
 	public AnalyticsCounter() {
 
-	}	
+	}
 
+	/**
+	 * 
+	 * @param inputPath  <String> a full or partial path to file with symptom
+	 *                   strings
+	 * @param outputPath <String> a full or partial path to a file for writing the
+	 *                   result of counting
+	 */
 	public AnalyticsCounter(String inputPath, String outputPath) {
 		this.inputPath = inputPath;
 		this.outputPath = outputPath;
 	}
-		
-	
+
 	// GETTERS and SETTERS
-	public 	void setISymptomReader(ISymptomReader iSymptomReader) {
+	public void setISymptomReader(ISymptomReader iSymptomReader) {
 		this.iSymptomReader = iSymptomReader;
 	}
-	public 	ISymptomReader getISymptomReader() {
+
+	public ISymptomReader getISymptomReader() {
 		return this.iSymptomReader;
 	}
-	
-	public 	void setISymptomCounter(ISymptomCounter iSymptomCounter) {
+
+	public void setISymptomCounter(ISymptomCounter iSymptomCounter) {
 		this.iSymptomCounter = iSymptomCounter;
 	}
-	public 	ISymptomCounter getISymptomCounter() {
+
+	public ISymptomCounter getISymptomCounter() {
 		return this.iSymptomCounter;
 	}
-	
-	public 	void setISymptomWriter(ISymptomWriter iSymptomWriter) {
+
+	public void setISymptomWriter(ISymptomWriter iSymptomWriter) {
 		this.iSymptomWriter = iSymptomWriter;
 	}
-	public 	ISymptomWriter getISymptomWriter() {
+
+	public ISymptomWriter getISymptomWriter() {
 		return this.iSymptomWriter;
 	}
 
-	public 	void setInputPath(String inputPath) {
+	public void setInputPath(String inputPath) {
 		this.inputPath = inputPath;
 	}
-	public 	String getinputPath() {
+
+	public String getInputPath() {
 		return this.inputPath;
 	}
-	
-	public 	void setOutPath(String outputPath) {
+
+	public void setOutPath(String outputPath) {
 		this.outputPath = outputPath;
 	}
-	public 	String getOutPath() {
-		return this.outputPath;
-	}	
-	
-}       
 
+	public String getOutPath() {
+		return this.outputPath;
+	}
+
+}
--- src/com/hemebiotech/analytics/CountAllSymptoms.java
+++ src/com/hemebiotech/analytics/CountAllSymptoms.java
@@ -2,31 +2,31 @@ package com.hemebiotech.analytics;
 
 import java.util.List;
 import java.util.TreeMap;
+
 /**
  * count the occurrence of ALL the symptoms and sorting alphabetically
  *
  */
-public class CountAllSymptoms implements ISymptomCounter{
-/**
- *   count the occurrence of ALL the symptoms and sort alphabetically
- *   
- *   @param listSymptoms : list of strings (the list of the symptoms)
- *   @return Object : a sorted collection of all symptoms and their occurrence 
- */
+public class CountAllSymptoms implements ISymptomCounter {
+	/**
+	 * count the occurrence of ALL the symptoms and sort alphabetically
+	 * 
+	 * @param listSymptoms : list of strings (the list of the symptoms)
+	 * @return Object : a sorted collection of all symptoms and their occurrence
+	 */
 	public Object countSymptoms(List<String> listSymptoms) {
 		TreeMap<String, Integer> treeMapSymptoms = new TreeMap<>();
-		
-		for (String symptom : listSymptoms){			
+
+		for (String symptom : listSymptoms) {
 			// incrementing counter of symptom
-			if  (treeMapSymptoms.containsKey(symptom)) {
-				treeMapSymptoms.put(symptom, treeMapSymptoms.get(symptom)+1);
+			if (treeMapSymptoms.containsKey(symptom)) {
+				treeMapSymptoms.put(symptom, treeMapSymptoms.get(symptom) + 1);
 			}
 			// adding the symptom in the map collection
-			else { 
+			else {
 				treeMapSymptoms.put(symptom, 1);
 			}
 		}
 		return treeMapSymptoms;
-	}	
+	}
 }
-
